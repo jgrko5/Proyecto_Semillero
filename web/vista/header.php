7a88ef93156d90bb399567c87b7be5efcf98529e
ccbernal38 <bernal_9219@hotmail.com>
2014-05-11 01:35:13 -0500

    Barra de menu

--- web/vista/header.php
+++ web/vista/header.php
@@ -8,9 +8,9 @@
 	<div id="contenedorHeader">
 		<div class="panelHeader">
 			<h1 id="logoStart">
-				<img id="banner" class="animated flip" style="width: 7.8%;position: absolute; z-index: 3;" src="../../web/imagenes/logo.png"  >
+				<img id="banner" class="animated flip" style="width: 6%;position: absolute; z-index: 3; margin-top: 18px;margin-left: 14px " src="../../web/imagenes/logo.png"  >
 			<center>
-				<img style="width: 100%;position: relative; z-index: 2;" src="../../web/imagenes/banner.jpg" >
+				<img style="width: 100%;position: relative; z-index: 2;" src="../../web/imagenes/banner.jpg"  >
 			</center><h1>
 		</div>
 
--- web/vista/panelDelegados.php
+++ web/vista/panelDelegados.php
@@ -1,99 +1,19 @@
 <?php
 
-	function getPanelDelegados()
+	function getPanelSesion()
 	{
 
 ?>
-<div id="menu">
-	<ul id>
-		<li class="nivel1 primera" tabindex="1">
-			<span class="nivel1">Opción 1</span>
-			<!--[if IE]><a href="#" class="nivel1ie">Opción 1<table class="falsa"><tr><td><![endif]-->
-			<ul>
-				<li class="primera">
-					<a href="http://www.google.es">Opción 1.1</a>
-				</li>
-				<li>
-					<a href="http://www.idplus.org">idplus.org</a>
-				</li>
-			</ul>
-			<!--[if IE]></td></tr></table></a><![endif]-->
-		</li>
-		<li class="nivel1" tabindex="2">
-			<span class="nivel1">Opción 2</span>
-			<!--[if IE]><a href="#" class="nivel1ie">Opción 2<table class="falsa"><tr><td><![endif]-->
-			<ul>
-				<li class="primera">
-					<a href="#">Opción 2.1</a>
-				</li>
-				<li>
-					<a href="http://www.google.es">Opción 2.2</a>
-				</li>
-				<li>
-					<a href="#">Opción 2.3</a>
-				</li>
-				<li>
-					<a href="#">Opción 2.4</a>
-				</li>
-				<li>
-					<a href="http://www.idplus.org">idplus.org</a>
-				</li>
-			</ul>
-			<!--[if IE]></td></tr></table></a><![endif]-->
-		</li>
-		<li class="nivel1" tabindex="3">
-			<span class="nivel1">Opción 3</span>
-			<!--[if IE]><a href="#" class="nivel1ie">Opción 3<table class="falsa"><tr><td><![endif]-->
-			<ul>
-				<li class="primera">
-					<a href="http://www.google.es">Opción 3.1</a>
-				</li>
-				<li>
-					<a href="#">Opción 3.2</a>
-				</li>
-				<li>
-					<a href="http://www.idplus.org">idplus.org</a>
-				</li>
-			</ul>
-			<!--[if IE]></td></tr></table></a><![endif]-->
-		</li>
-		<li class="nivel1" tabindex="4">
-			<span class="nivel1">Opción 4</span>
-			<!--[if IE]><a href="#" class="nivel1ie">Opción 4<table class="falsa"><tr><td><![endif]-->
-			<ul>
-				<li class="primera">
-					<a href="#">Opción 4.1</a>
-				</li>
-				<li>
-					<a href="#">Opción 4.2</a>
-				</li>
-				<li>
-					<a href="#">Opción 4.3</a>
-				</li>
-				<li>
-					<a href="http://www.idplus.org">idplus.org</a>
-				</li>
-			</ul>
-			<!--[if IE]></td></tr></table></a><![endif]-->
-		</li>
-		<li class="nivel1" tabindex="5">
-			<span class="nivel1">Opción 5</span>
-			<!--[if IE]><a href="#" class="nivel1ie">Opción 5<table class="falsa"><tr><td><![endif]-->
-			<ul>
-				<li class="primera">
-					<a href="#">Opción 5.1</a>
-				</li>
-				<li>
-					<a href="#">Opción 5.2</a>
-				</li>
-				<li>
-					<a href="http://www.idplus.org">idplus.org</a>
-				</li>
-			</ul>
-			<!--[if IE]></td></tr></table></a><![endif]-->
-		</li>
-	</ul>
-</div>
+<nav>
+        <!-- Listado de Navegación -->
+        <ul class="menu">  
+            <li><a href="#">Home</a></li>  
+            <li><a href="#">Quienes Somos</a></li>  
+            <li><a href="#">Que Hacemos</a></li>
+            <li><a href="#">Clientes</a></li>
+            <li><a href="#">Club Coolaboro</a></li> 
+        </ul>    
+</nav>
 <?php
 }
 ?>
