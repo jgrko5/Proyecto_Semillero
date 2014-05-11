24e2fc42636f0f75b1d3606cb233651ec06fb9d7
ccbernal38 <bernal_9219@hotmail.com>
2014-05-11 17:49:12 -0500

    comit

--- web/vista/menuIzquierdo.php
+++ web/vista/menuIzquierdo.php
@@ -8,6 +8,70 @@
 	<nav>
 	<ul>
 		<li class="mheader">
+			Nombre facultad88
+		</li>
+		
+		<li>
+			<a href="#"><span>Estudiante</span></a>
+			<ul>
+				<li class="lupper">
+					<a href="#nolink">Nikon Lenses</a>
+				</li>
+				<li>
+					<a href="#nolink">Canon Lenses</a>
+				</li>
+				<li>
+					<a href="#nolink">Sigma Lenses</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><span>Evento</span></a>
+		</li>
+		<li>
+			<a href="#"><span>Grupo de investigación</span></a>
+		</li>
+		<li>
+			<a href="#"><span>Materia</span></a>
+		</li>
+		<li>
+			<a href="#"><span>Premio</span></a>
+			<ul>
+				<li class="lupper">
+					<a href="#nolink">Nikon Lenses</a>
+				</li>
+				<li>
+					<a href="#nolink">Canon Lenses</a>
+				</li>
+				<li>
+					<a href="#nolink">Sigma Lenses</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#"><span>Proyecto de investigación</span></a>
+		</li>
+		<li class="last">
+			<a href="#"><span>Tutor</span></a>
+		</li>
+		
+	</ul>
+</nav>
+</div>
+
+<?php
+}
+?>
+<?php
+    function getMenuIzquierdoVice()
+	{
+
+?>
+
+<div id="indice" class="flyoutmenu" >
+	<nav>
+	<ul>
+		<li class="mheader">
 			Facultades
 		</li>
 		
