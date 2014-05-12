364e12eeee7266a84998ecf24634660e3a5e0580
crisdanielcm <crisdanielcm@hotmail.com>
2014-05-12 06:28:04 -0500

    evento

--- web/vista/buscarEstudiante.php
+++ web/vista/buscarEstudiante.php
@@ -16,15 +16,34 @@ getImports();
 			<section id="estudiante">
 				<article>
 					<header>
-						<h6>Buscar estudiante</h6></br>
+						<h6>Buscar estudiante</h6>
 					</header>
-					
+
 					<div id="formulario">
-						<form action="../controlador/registrarProyecto.php" method="post" ></form>
+						<form action="../controlador/registrarProyecto.php" method="post" >
+							<div class="etiqueta">
+								<label>Ingrese el número de identificación:</label>
+							</div></br>
+							<div class="componente">
+								<input class="textField" type="text" name="númeroId" required="required" placeholder="Número de identificación"/>
+							</div>
+							<div align="center">
+								<input class="button" type="submit" value="Buscar"/>
+							</div></br>
 							<center>
-								
+							<div id="tabla">
+								<table style="border:1px solid #666;">
+									<tr>
+										<td style="width:50%;border:1px inset #666;">Nombres y apellidos</td>
+										<td style="width:30%;border:1px inset #666;">Documento</td>
+										<td style="width:40%;border:1px inset #666;">Semestre</td>
+										<td style="width:40%;border:1px inset #666;">Correo</td>
+									</tr>
+								</table>
 							</center>
-						</div>
+							</div>
+						</form>
+					</div>
 
 				</article>
 			</section>
