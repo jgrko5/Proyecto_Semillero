827ceca7a9c868c0e893deb8959e7df21cb8bb4b
crisdanielcm <crisdanielcm@hotmail.com>
2014-05-13 13:30:33 -0500

    cristian

---
+++ web/vista/buscarEvento.php
@@ -0,0 +1,59 @@
+<?php
+include_once ("imports.php");
+include_once ("header.php");
+include_once ("footer.php");
+getImports();
+?>
+<body>
+	<div id="main" class="wrapper">
+		<?php
+		getHeaderStart();
+		getPanelSesion();
+		getMenuIzquierdo();
+		?>
+		<div id="contenido">
+			<section id="estudiante">
+				<article>
+					<header>
+						</br><h6>Buscar evento</h6>
+					</header>
+
+					<div id="formulario">
+						<form action="../controlador/registrarProyecto.php" method="post" ></form>
+						<center>
+							<div class="etiqueta">
+								<label>Ingrese el nombre del evento</label>
+							</div></br>
+
+							<div class="componente">
+								<input class="textfield" type="text" name="tipo" required="required" placeholder="nonmbre evento" />
+							</div>
+
+							<div align="center">
+								<input class="button" type="submit" value="Buscar"/>
+							</div></br>
+							<header>
+								<h1>Estudiantes</h1>
+							</header>
+
+							<div class="tabla">
+								<table style="border:1px solid #666;">
+									<tr>
+										<td style="border:1px inset #666;">Nombre</td>
+										<td style="border:1px inset #666;">Ciudad</td>
+										<td style="border:1px inset #666;">Año</td>
+									</tr>
+								</table>
+							</div></br>
+						</center>
+					</div>
+				</article>
+			</section>
+		</div
+
+	<?php 
+		getFooter();
+	?>
+	</div>
+</body>
+</html>
