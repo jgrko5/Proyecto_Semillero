ec835a7e5e7b35463e8ca1ce8b7d1050fe561bf7
crisdanielcm <crisdanielcm@hotmail.com>
2014-05-13 11:14:56 -0500

    actualizar

--- web/vista/actualizarProyecto.php
+++ web/vista/actualizarProyecto.php
@@ -12,52 +12,53 @@ getImports();
 		getMenuIzquierdo();
 		?>
 		<div id="contenido">
-		<section id="proyecto">
-			<article>
+			<section id="proyecto">
+				<article>
 
-				<header>
-					</br><h6>Actualizar proyecto</h6>
-				</header>
+					<header>
+						</br><h6>Actualizar proyecto</h6>
+					</header>
+					<div id="formulario">
+						<form action="../controlador/registrarProyecto.php" method="post">
 
-				<div id="formulario">
-					<form action="../controlador/registrarProyecto.php" method="post">
-						<center>
-							
 							<div class="etiqueta">
 								<label>Título:</label>
-							</div>
+							</div></br>
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" required="required" placeholder="Ingrese el título"/>
 							</div></br>
 
 							<div class="etiqueta">
 								<label>Gasto efectivo:</label>
-							</div>
+							</div></br>
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" required="required" placeholder="Ingrese el gasto efectivo"/>
 							</div></br>
 							<div class="etiqueta">
 								<label>Duración:</label>
-							</div>
+							</div></br>
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" required="required" placeholder="Ingrese la duración"/>
 							</div></br>
 							<div class="etiqueta">
 								<label>Fecha inicio:</label>
-							</div>
+							</div></br>
 							<div class="componente">
 								<input type="date" class="textField"/>
 							</div></br></br>
-				</div></br>
-				<div align="center">
-					<input class="button" type="submit" value="Actualizar"/>
-				</div></br>
-				</center>
-				</form>
-	</div>
-	</article>
-	</section>
-	</div>
+
+							<div align="center">
+
+								<input class="button" type="submit" value="Actualizar"/>
+
+							</div></br>
+
+						</form>
+					</div>
+
+				</article>
+			</section>
+		</div>
 	</div>
 </body>
 </html>
