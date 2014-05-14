ffb236921edd5babdad4a7f6379ae7ae9293c1d7
nia1124 <nia-5@hotmail.com>
2014-05-14 00:21:26 -0500

    pagina asignar premio, y modificacion de las margenes del boton buscar en css

---
+++ web/vista/asignarPremio.php
@@ -0,0 +1,67 @@
+<?php
+include_once ("imports.php");
+include_once ("header.php");
+include_once ("footer.php");
+getImports();
+?>
+<body>
+	<div id="main" class="wrapper">
+		<?php getheaderstart();
+		getPanelSesion();
+		getMenuIzquierdo();
+		?>
+		<div id="contenido">
+			<section id="premio">
+				<article>
+					<header>
+						</br><h6>Asignar premio</h6>
+					</header>
+					<div id="formulario">
+						<form action="../controlador/asirnarPremio.php" method="post">
+							
+							<div class="etiqueta">
+								<label>Ingrese el código del estudiante:</label>
+							</div>
+
+							<div class="componente">
+								<input class="textField" type="text" name="tipo" required="required" placeholder="Código">
+							</div>
+							
+							<div align="left">
+								<input class="button" type="submit" value="Buscar" />
+							</div>
+							
+							<div class="etiqueta">
+								<label>Nombres y apellidos:</label>
+							</div></br>
+
+							<div class="componente">
+								<input class="textField" type="text" name="tipo" required="required">
+							</div></br>
+							
+							<div class="etiqueta">
+								<label>Premios</label>
+							</div></br>
+
+							<div class="componente">
+								<select class="select" title="Premios">
+									<option>uno</option>
+								</select>
+							</div></br></br>
+
+							<div align="center">
+								<input class="button" type="submit" value="Asignar" />
+							</div></br>
+
+					</div>
+					</form>
+		</div>
+		</article>
+		</section>
+	</div>
+	<?php
+	getFooter();
+	?>
+	</div>
+</body>
+</html>
