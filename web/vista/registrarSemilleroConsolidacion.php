3184f75dfb775e0f5c3e68e9bcb876355bb20874
nia1124 <nia-5@hotmail.com>
2014-05-14 23:31:30 -0500

    arreglo pagina registrar semillero ejecucion

--- web/vista/registrarSemilleroConsolidacion.php
+++ web/vista/registrarSemilleroConsolidacion.php
@@ -19,72 +19,70 @@ getImports();
 					</header>
 					<div id="formulario">
 						<form action="../controlador/registrarSemilleroConsolidacion.php" method="post">
-							
+
 							<div class="etiqueta">
 								<label>Grupo de investigación:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<select class="select" title="Grupos de investigación">
 									<option>sinfoci</option>
 								</select>
 							</div></br>
-							
+
 							<div class="etiqueta">
 								<label>Proyecto de investigación:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<select class="select" title="Proyecto de investigación">
-									<option> </option>
+									<option></option>
 								</select>
 							</div></br>
-							
+
 							<div class="etiqueta">
 								<label>Año:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" placeholder="Año"/>
 							</div></br>
-							
+
 							<div class="etiqueta">
 								<label>Período:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" placeholder="Período"/>
 							</div></br>
-										
-							
+
 							<div class="etiqueta">
 								<label>Nota:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" placeholder="Nota"/>
 							</div></br>
-							
+
 							<div class="etiqueta">
 								<label>Horas empleadas del docente:</label>
 							</div></br>
-							
+
 							<div class="componente">
 								<input class="textField" type="text" name="tipo" placeholder="Horas empleadas docente"/>
 							</div></br>
-							
+
 							<div class="etiqueta">
 								<label>Homologación:</label>
 							</div></br>
-							
+
 							<div class="componente">
-							<input type="checkbox" id="homologacion" value="3" onChange="total()"/> 
-							</div></br>
-							
+								<input type="checkbox" id="homologacion" value="3" onChange="total()"/>
+							</div></br></br>
+
 							<div align="center">
 								<input class="button" type="submit" value="Registrar" />
 							</div></br>
-							
 						</form>
 					</div>
 				</article>
