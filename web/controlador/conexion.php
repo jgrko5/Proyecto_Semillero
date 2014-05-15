92684537eeee225af3195cc81ebbdfbbea2ff57f
crisdanielcm <crisdanielcm@hotmail.com>
2014-05-15 00:24:36 -0500

    12:24 am

--- web/controlador/conexion.php
+++ web/controlador/conexion.php
@@ -4,4 +4,23 @@
 	$pass = "semilleros";
 	$db = "xe";
 	
+	// Conecta al servicio XE (esto es, una base de datos) en la mÃ¡quina "localhost"
+$conn = oci_pconnect('admin', 'semilleros', 'localhost/xe');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM employees');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
 ?>
