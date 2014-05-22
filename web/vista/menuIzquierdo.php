3a735afd480d9f5875a431a6a26e632872b160cb
crisdanielcm <crisdanielcm@hotmail.com>
2014-05-22 02:45:04 -0500

    menuVicerrectoria. Faltan muchas cosas

--- web/vista/menuIzquierdo.php
+++ web/vista/menuIzquierdo.php
@@ -40,6 +40,7 @@ include_once ("menuDerecho.php");
 					?>
 						</a>
 					</li>
+				</ul>
 			</li>
 			<li>
 				<a href="#"><span>Eventos</span></a>
@@ -70,7 +71,7 @@ include_once ("menuDerecho.php");
 					<li>
 						<a href="actualizarGrupoInvestigacion.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="asigarGrupoInvestigacion.php">Asignar estudiante</a>
 					</li>
@@ -78,7 +79,7 @@ include_once ("menuDerecho.php");
 					<li class="lupper">
 						<a href="buscarGrupoInvestigacion.php">Buscar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="registrarGrupoInvestigacion.php">Registrar</a>
 					</li>
@@ -86,18 +87,35 @@ include_once ("menuDerecho.php");
 				</ul>
 
 			</li>
-			
+
+			<li class="last">
+				<a href="#"><span>Espacios académicos</span></a>
+				<ul>
+					<li>
+						<a href="actualizarEspacioAcademico.php">Actualizar</a>
+					</li>
+
+					<li class="lupper">
+						<a href="listarEspaciosAcademicos.php">Lista espacios académicos</a>
+					</li>
+
+					<li>
+						<a href="registrarEspaciosAcademicos.php">Registrar</a>
+					</li>
+				</ul>
+			</li>
+
 			<li class="last">
 				<a href="#"><span>Premios</span></a>
 				<ul>
 					<li>
 						<a href="actualizarPremios.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="asigarPremios.php">Asignar estudiante </a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="listarPremios.php">Lista premios</a>
 					</li>
@@ -107,53 +125,53 @@ include_once ("menuDerecho.php");
 					</li>
 				</ul>
 			</li>
-			
+
 			<li>
 				<a href="#"><span>Proyecto de investigación</span></a>
 				<ul>
 					<li>
 						<a href="actualizarProyecto.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="asigarProyecto.php">Asignar estudiantes</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="buscarProyecto.php">Buscar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="registrarProyecto.php">Registrar</a>
 					</li>
 				</ul>
 			</li>
-			
+
 			<li>
 				<a href="#"><span>Semillero en consolidación</span></a>
 				<ul>
 					<li>
 						<a href="actualizarSemilleroConsolidacion.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="listarSemilleroConsolidacion.php">Lista proyectos consolidación</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="registrarSemilleroConsolidacion.php">Registrar</a>
 					</li>
 
 				</ul>
 			</li>
-			
+
 			<li class="last">
 				<a href="#"><span>Semillero en ejecución</span></a>
 				<ul>
 					<li>
 						<a href="actualizarSemilleroEjecucion.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="listarSemilleroEjecucion.php">Lista proyectos ejecución</a>
 					</li>
@@ -167,19 +185,19 @@ include_once ("menuDerecho.php");
 			<li class="last">
 				<a href="#"><span>Tutor</span></a>
 				<ul>
-					
+
 					<li>
 						<a href="actualizarTutor.php">Actualizar</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="asigarutor.php">Asignar proyecto de investigación</a>
 					</li>
-					
+
 					<li class="lupper">
 						<a href="buscarTutor.php">Buscar</a>
 					</li>
-					
+
 					<li>
 						<a href="registrarTutor.php">Registrar</a>
 					</li>
@@ -206,94 +224,54 @@ function getMenuIzquierdoVice()
 			<li class="mheader">
 				Inicio
 			</li>
-
 			<li>
-				<a  href="#"><span >Facultad</span></a>
+				<a href="#"><span>Administración de cuentas</span></a>
 				<ul>
 					<li class="lupper">
-						<a href="buscarEstudiante.php">Ingenieria</a>
+						<a href="enn.php" >En construcción</a>
 					</li>
 					<li>
-						<a href="actualizarEstudiante.php">Modificar</a>
-					</li>
-					<li>
-						<a href="registrarEstudiante.php">Registrar</a>
-					</li>
-
-				</ul>
-			</li>
-			<li>
-				<a href="#"><span>Evento</span></a>
-				<ul>
-					<li class="lupper">
-						<a href="registrarEvento.php" >Registrar</a>
-					</li>
-					<li>
-						<a href="asignarEvento.php">Asignar</a>
-					</li>
-
-				</ul>
-			</li>
-			<li>
-				<a href="#"><span>Grupo de investigación</span></a>
-				<ul>
-					<li class="lupper">
-						<a href="actualizarGrupoInvestigacion.php">Actualizar</a>
-					</li>
-					<li>
-						<a href="buscarGrupoInvestigacion.php">Buscar</a>
-					</li>
-					<li>
-						<a href="registrarGrupoInvestigacion.php">Registrar</a>
-					</li>
-				</ul>
-
-			</li>
-			<li>
-				<a href="#"><span>Materia</span></a>
-			</li>
-			<li>
-				<a href="#"><span>Premio</span></a>
-				<ul>
-					<li class="lupper">
-						<a href="registrarPremio.php">Registrar</a>
+						<a href="enn.php">En cons...</a>
 					</li>
 				</ul>
 			</li>
 			<li>
-				<a href="#"><span>Proyecto de investigación</span></a>
+				<a  href="#"><span >Facultades</span></a>
 				<ul>
+					<li>
+						<a href="inicioCienciasAdmin.php">Ciencias administrativas y económicas</a>
+					</li>
+					<li>
+						<a href="inicioCienciasAgro.php">Ciencias agroindustriales</a>
+					</li>
+					<li>
+						<a href="inicioCienciasBasicas.php">Ciencias básicas y tecnológicas</a>
+					</li>
+					<li>
+						<a href="inicioCienciasSalud.php">Ciencias de la salud</a>
+					</li>
+					<li>
+						<a href="inicioCienciasHumanas.php">Ciencias humanas y bellas artes</a>
+					</li>
+					<li>
+						<a href="inicioCienciasEducacion.php">Educación</a>
+					</li>
 					<li class="lupper">
-						<a href="actualizarProyecto.php">Actualizar</a>
-					</li>
-					<li>
-						<a href="registrarProyecto.php">Registrar</a>
-					</li>
-					<li>
-						<a href="buscarProyecto.php">Registrar</a>
+						<a href="inicioIngenieria.php">Ingenieria</a>
 					</li>
 				</ul>
 			</li>
-			<li class="last">
-				<a href="#"><span>Tutor</span></a>
+			<li>
+				<a href="#"><span>Reportes</span></a>
 				<ul>
-					<li>
-						<a href="actualizarTutor.php">Actualizar</a>
-					</li>
-					
-					<li>
-						<a href="asignarTutor.php">Asignar</a>
-					</li>
-					
 					<li class="lupper">
-						<a href="buscarTutor.php">Buscar</a>
+						<a href="enn.php" >En construcción</a>
 					</li>
 					<li>
-						<a href="registrarTutor.php">Registrar</a>
+						<a href="enn.php">En cons...</a>
 					</li>
 				</ul>
 			</li>
-
 		</ul>
 	</nav>
 </div>
