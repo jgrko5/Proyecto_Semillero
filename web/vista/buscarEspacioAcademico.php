16c92558efe7f9ced7903d34d27f679f0839a185
nia1124 <nia-5@hotmail.com>
2014-06-02 21:14:57 -0500

    ,,

--- web/vista/buscarEspacioAcademico.php
+++ web/vista/buscarEspacioAcademico.php
@@ -2,11 +2,10 @@
 include_once ("imports.php");
 include_once ("header.php");
 include_once ("footer.php");
-<<<<<<< HEAD
+
 include_once ("../controlador/buscarEspacioAcademico.php");
-=======
+
 include_once ("../controlador/buscarEspaciosAcademicos.php");
->>>>>>> 066fb10c156c71414185a87b0ef80c17e067649d
 getImports();
 ?>
 <body onload="tunCalendario();">
