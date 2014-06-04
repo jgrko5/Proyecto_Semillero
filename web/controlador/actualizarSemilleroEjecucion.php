a101374ff0d86e93b0079d88dae9a5fa3f67fb32
ccbernal38 <bernal_9219@hotmail.com>
2014-06-04 00:54:53 -0500

    b

---
+++ web/controlador/actualizarSemilleroEjecucion.php
@@ -0,0 +1,36 @@
+<?php
+include_once ('oracle.php');
+
+session_start();
+
+$horas = $_POST['horas'];
+$anio = $_POST['anio'];
+$periodo = $_POST['periodo'];
+$nota = $_POST['nota'];
+
+$conexion = conectar();
+
+$stid = oci_parse($conexion, 'UPDATE SEMILLEROS_EJECUCION SET (año = :anio, horasdocente = :horas, nota = :nota, 
+                                                                  periodo = :periodo) WHERE id=:id');
+
+oci_bind_by_name($stid, ':anio', $anio);
+oci_bind_by_name($stid, ':horas', $horas);
+oci_bind_by_name($stid, ':nota', $nota);
+oci_bind_by_name($stid, ':periodo', $periodo);
+oci_bind_by_name($stid, ':id', $_SESSION['idSE']);
+
+$r = oci_execute($stid);
+
+
+oci_free_statement($stid);
+
+oci_close($conexion);
+
+unset($_SESSION['idSE'],$_SESSION['codProSE'],$_SESSION['proyectoSE'],$_SESSION['anioSE'],$_SESSION['periodoSE'],$_SESSION['notaSE'],$_SESSION['horasSE']);
+
+echo "<script type='text/javascript'>
+    alert('Proyecto en ejecución actualizado con exito'); 
+    document.location.href='../vista/buscarSemilleroEjecucion.php';
+    </script>";
+
+?>
--- web/controlador/buscarProyectoConsolidacion.php
+++ web/controlador/buscarProyectoConsolidacion.php
@@ -1,44 +1,32 @@
 <?php
-include_once('oracle.php');
+include_once ('oracle.php');
 
 session_start();
 
 $codigoPC = $_POST['codPC'];
 
 $conexion = conectar();
-echo $codigoPC;
 if ($_SESSION['seleccion'] == 1) {
-	$stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_INGENIERIA where(codigo =:codigoPC)');
-}else
-   if ($_SESSION['seleccion'] == 2) {
-		$stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_EDUCACION where codigo =:codigoPC');
-   }
-   else
-	 if ($_SESSION['seleccion'] == 21) {
-		   $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_SALUD where(codigo =:codigoPC)');
-	 }
-	 else
-	   if ($_SESSION['seleccion'] == 22) {
-			$stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_BASICAS where(codigo =:codigoPC)');
-	   }
-	   else
-		 if ($_SESSION['seleccion'] == 23) {
-			  $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_AGROINDUSTRIA where(codigo =:codigoPC)');
-		 }
-		 else
-	  	   if ($_SESSION['seleccion'] == 24) {
-				$stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_BELLAS_ARTES where(codigo =:codigoPC)');
-	   	   }
-		   else
-	  		 if ($_SESSION['seleccion'] == 25) {
-				  $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_ECONOMICA where(codigo =:codigoPC)');
-	   		 	}
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_INGENIERIA where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 2) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_EDUCACION where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 21) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_SALUD where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 22) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_BASICAS where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 23) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_AGRO where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 24) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_BELLAS where codigo = :codigoPC');
+} else if ($_SESSION['seleccion'] == 25) {
+    $stid = oci_parse($conexion, 'select * from CRUD_CONSOLIDACION_ECONOMICA where codigo = :codigoPC');
+}
 
 oci_bind_by_name($stid, ':codigoPC', $codigoPC);
 
 oci_execute($stid);
 
-$emergenteProC = "<div id=" . '"openModal"' . " class=" . '"modalDialog"' . "><div><a href=" . '"#close"' . " title=" . '"Close"' . " class=" . '"close"' . ">X</a><header class=".'"modalDialogHeader"';
+$emergenteProC = "<div id=" . '"openModal"' . " class=" . '"modalDialog"' . "><div><a href=" . '"#close"' . " title=" . '"Close"' . " class=" . '"close"' . ">X</a><header class=" . '"modalDialogHeader"';
 $emergenteProC .= "><h6>Información de proyecto de investigación en consolidacion</h6></header>";
 
 if ($row = oci_fetch_array($stid)) {
@@ -57,24 +45,33 @@ if ($row = oci_fetch_array($stid)) {
     if ($row[4] == "") {
         $row[4] = "No registra";
     }
-    $emergenteProC.= "<div class=".'"etiquetaE"'."style=".'"font-weight: bold;"'."><label>Codigo:</label></div>";
-    $emergenteProC.= "<div class=".'"etiquetaE"'."><label>".$row[0]."</label></div>";
-    $emergenteProC.= "<div class=".'"etiquetaE"'."style=".'"font-weight: bold;"'."><label>Titulo:</label></div>".
-    "<div class=".'"etiquetaE"'."><label>".$row[1]."</label></div></br>
-    <div class=".'"etiquetaE"'."style=".'"font-weight: bold;"'."><label>Gasto efectivo:</label></div>
-    <div class=".'"etiquetaE"'."><label>".$row[2]."</label></div></br>
-    <div class=".'"etiquetaE"'."style=".'"font-weight: bold;"'."><label>Duración:</label></div>
-    <div class=".'"etiquetaE"'."><label>".$row[3]."</label></div></br>
-    <div class=".'"etiquetaE"'."style=".'"font-weight: bold;"'."><label>Fecha de inicio:</label></div>
-    <div class=".'"etiquetaE"'."><label>".$row[4]."</label></div></br>
-    <div class=".'"etiquetaE"'."><a hrfe=".'"actualizarSemilleroConsolidacion.php"'."</a></div></br></br></br>";
     
+    $_SESSION['idSE']=$row[8];
+    $_SESSION['codProSE']=$row[6];
+    $_SESSION['proyectoSE']=$row[7];
+    $_SESSION['anioSE']=$row[5];
+    $_SESSION['periodoSE']=$row[3];
+    $_SESSION['notaSE']=$row[0];
+    
+    $emergenteProC .= "<div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;"' . "><label>Codigo:</label></div>";
+    $emergenteProC .= "<div class=" . '"etiquetaE"' . "><label>" . $row[0] . "</label></div>";
+    $emergenteProC .= "<div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;"' . "><label>Titulo:</label></div>" . "<div class=" . '"etiquetaE"' . "><label>" . $row[1] . "</label></div></br>
+    <div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;"' . "><label>Gasto efectivo:</label></div>
+    <div class=" . '"etiquetaE"' . "><label>" . $row[2] . "</label></div></br>
+    <div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;"' . "><label>Duración:</label></div>
+    <div class=" . '"etiquetaE"' . "><label>" . $row[3] . "</label></div></br>
+    <div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;"' . "><label>Fecha de inicio:</label></div>
+    <div class=" . '"etiquetaE"' . "><label>" . $row[4] . "</label></div></br>";
+
+    if ($_SESSION['idFacultad'] == 83) {
+        $emergenteProC .= "<div class=" . '"etiquetaE"' . "><a href=" . '"actualizarSemilleroConsolidacion.php"' . ">Actualizar informacion</a></div></br>";
+    }
+    $emergenteProC .= "</br></br>";
+
+} else {
+    $emergenteProC .= $codigoPC."<div class=" . '"etiquetaE"' . "style=" . '"font-weight: bold;font-size:16px"' . "><label>No se encontraron coincidencias, por favor intente nuevamente</label></div></br>";
 }
-else
-{
-$emergenteProC.= "<div class=".'"etiquetaE"'."style=".'"font-weight: bold;font-size:16px"'."><label>No se encontraron coincidencias, por favor intente nuevamente</label></div></br>";    
-}
-$emergenteProC.="</div></div>";
+$emergenteProC .= "</div></div>";
 
 oci_free_statement($stid);
 
